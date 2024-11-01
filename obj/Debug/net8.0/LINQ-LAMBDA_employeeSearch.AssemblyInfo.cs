88e610c9aa5846271090012e51a899764d634f94
LeuchDiogo <diogo14leuch@gmail.com>
2024-11-01 13:47:13 -0300

    organizing project

--- obj/Debug/net8.0/LINQ-LAMBDA_employeeSearch.AssemblyInfo.cs
+++ obj/Debug/net8.0/LINQ-LAMBDA_employeeSearch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ-LAMBDA_employeeSearch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a416402bc337bb962eeaf476dfad6609f8b7086a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ-LAMBDA_employeeSearch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ-LAMBDA_employeeSearch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
